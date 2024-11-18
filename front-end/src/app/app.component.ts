f071a7f4f1a7442b489c92e5c40295f3046d970e
Fabricio Nuñez <78011988+Fabriciond@users.noreply.github.com>
2024-11-18 10:45:05 -0700

    init config

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Router } from 'express';
 @Component({
   selector: 'app-root',
   standalone: true,
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { PageEventsComponent } from './page-events/page-events.component';
+import { PagePlansComponent } from './page-plans/page-plans.component';
+import { PanelAdminComponent } from './panel-admin/panel-admin.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path:'register', component: RegisterComponent},
+    {path: 'events', component: PageEventsComponent},
+    {path:'plans', component: PagePlansComponent},
+    {path: '', component: HomeComponent},
+
+    {path: 'admin', component: PanelAdminComponent},
+    {path: 'events/add', component: CreateEventComponent}
+];
