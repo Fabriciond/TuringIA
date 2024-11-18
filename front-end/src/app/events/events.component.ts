e4846b2267b0369ac197f8e143e010be38b71fce
Fabricio Nuñez <78011988+Fabriciond@users.noreply.github.com>
2024-11-18 10:47:00 -0700

    change client

--- front-end/src/app/events/events.component.ts
+++ front-end/src/app/events/events.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { EventsService } from '../events.service';
+
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css'
+  styleUrl: './events.component.css',
 })
 export class EventsComponent {
-
+  title = 'front-end';
+  events: any = [];
+  constructor(private eventsService: EventsService) {
+    
+  }
+  ngOnInit() {
+    this.events = this.eventsService.events;
+  }
 }
